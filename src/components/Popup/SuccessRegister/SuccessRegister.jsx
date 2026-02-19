7ffbea6dbc109c10e8d382c610d4fb9e116200ee
Alexander Artemiev <alexanderma1983@gmail.com>
2026-02-19 13:17:50 -0500

    Actualizacion frontend

--- src/components/Popup/SuccessRegister/SuccessRegister.jsx
+++ src/components/Popup/SuccessRegister/SuccessRegister.jsx
@@ -1,10 +1,15 @@
 import './SuccessRegister.css';
 
-export function SuccessRegister(title, onClose) {
+export function SuccessRegister(title, onClose, handleSuccessRegister) {
     const handleButtonClick = () => {
-
+              if (handleSuccessRegister) {
+                  handleSuccessRegister();
+              }
+              if (onClose) {
+                  onClose();
+              }
     }
-  
+    
     return (
     <>
       <h2 
@@ -12,7 +17,7 @@ export function SuccessRegister(title, onClose) {
       id="popup--succes-title">{title}
       </h2>
       <a className="popup__succes--link" onClick={handleButtonClick}>
-
+        Iniciar sesión
       </a>
       
     </>
