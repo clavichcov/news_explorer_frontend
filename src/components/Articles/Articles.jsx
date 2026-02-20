281b81c4b295dda2c6b3d0ad24e1d1e00da438c7
Alexander Artemiev <alexanderma1983@gmail.com>
2026-02-20 14:38:59 -0500

    Actualizacion frontend

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -140,7 +140,7 @@ export function Articles({ isLoggedIn, onLogout, currentPath }) {
                                         isLoggedIn={isLoggedIn} 
                                         onLogout={onLogout} 
                                         currentPath={currentPath}
-                                        onSaveArticle={onSaveArticle(card)}
+                                        onSaveArticle={() => onSaveArticle(card)}
                                     />
                                 ))}
                             </div>
