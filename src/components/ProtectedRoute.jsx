d45dd666892feb63093703698b2bd99b6e83cd3d
Alexander Artemiev <alexanderma1983@gmail.com>
2026-02-22 13:31:42 -0500

    Actualizacion frontend

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { Preloader } from "./Preloader";
+import { Preloader } from "./Preloader/Preloader.jsx";
 import CurrentUserContext from "../contexts/CurrentUserContext";
 
 
